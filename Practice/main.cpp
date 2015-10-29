ea5e96acb8500ba109d0b6ade5fd53399bd742b6
Peter Sung <petersung21@gmail.com>
2015-10-28 20:16:29 -0400

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Eric Kyungwoo Sung on 2015-10-28.
+//  Copyright (c) 2015 Peter Hyungrok Sung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
