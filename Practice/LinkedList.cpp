bc42763c08c03714824d0bc7f607c6f3142ea5b7
Peter Sung <petersung21@gmail.com>
2015-10-31 20:37:34 -0400

    first commit

---
+++ Practice/LinkedList.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedList.cpp
+//  Practice
+//
+//  Created by Eric Kyungwoo Sung on 2015-10-31.
+//  Copyright (c) 2015 Peter Hyungrok Sung. All rights reserved.
+//
+
+#include "LinkedList.h"
---
+++ Practice/LinkedList.h
@@ -0,0 +1,14 @@
+//
+//  LinkedList.h
+//  Practice
+//
+//  Created by Eric Kyungwoo Sung on 2015-10-31.
+//  Copyright (c) 2015 Peter Hyungrok Sung. All rights reserved.
+//
+
+#ifndef __Practice__LinkedList__
+#define __Practice__LinkedList__
+
+#include <stdio.h>
+
+#endif /* defined(__Practice__LinkedList__) */
